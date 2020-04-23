0516de2e6947a7e3e830244e5ff6e586a318f433
rpedraza01 <rpedraza01@gmail.com>
2020-04-22 20:33:35 -0700

    improved stats roll randomness/probability

--- dnd_rpg_character_generator.py
+++ dnd_rpg_character_generator.py
@@ -7,12 +7,31 @@ import random
 # A summary of the new character is generated (maybe create a CSV or PDF of the summary?)
 # The script asks if the user would like to try again or end the program
 
+def dice_roll_func():
+	dice_rolls = [random.randint(1, 6) for _ in range(4)]
+	dice_rolls.sort(reverse = True)
+	return sum(dice_rolls[:3])
+
+
 while True:
 	print("Welcome mortal to the Dungeons and Dragons Random Character Generator! Proceed if you dare!")
 	user_char_name = input("What shall your hero be called? > ")
 	print(f"{user_char_name} is a strange name. Might you be from another land? \n Now let's determine your hero's class.")
 
-	char_class_list = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
+	char_class_list = [
+		"Barbarian",
+		"Bard",
+		"Cleric",
+		"Druid",
+		"Fighter",
+		"Monk",
+		"Paladin",
+		"Ranger",
+		"Rogue",
+		"Sorcerer",
+		"Warlock",
+		"Wizard"
+		]
 	user_class_pick = input("Would you like to pick your hero's class? If not, I will choose for you. > ")
 	if user_class_pick != "yes":
 		user_class_pick = random.choice(char_class_list)
@@ -21,24 +40,56 @@ while True:
 		user_class_pick = input("Please pick from the following list: \n Barbarian \n Bard \n Cleric \n Druid \n Fighter \n Monk \n Paladin \n Ranger \n Rogue \n Sorcerer \n Warlock \n Wizard > ")
 		print(f"You've selected {user_class_pick}. \n Next, we shall pick your hero's race.")
 
-	char_race_list = ["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"]
+	char_race_list = [
+		"Dwarf",
+		"Elf",
+		"Halfling",
+		"Human",
+		"Dragonborn",
+		"Gnome",
+		"Half-Elf",
+		"Half-Orc",
+		"Tiefling"
+		]
 	user_race_pick = input("Would you like to pick your hero's race? If not, I will choose for you. > ")
 	if user_race_pick != "yes":
 		user_race_pick = random.choice(char_race_list)
-		print(f"Your hero's race shall be {user_race_pick}! \n Next, we shall randomly generate your hero's base attributes.")
+		print(f"Your hero's race shall be {user_race_pick}! \n Next, we shall pick your hero's background.")
 	else:
 		user_race_pick = input("Please pick from the following list: \n Dwarf \n Elf \n Halfling \n Human \n Dragonborn \n Gnome \n Half-Elf \n Half-Orc \n Tiefling > ")
-		print(f"You've selected {user_race_pick}. \n Next, we shall randomly generate your hero's base attributes.")
+		print(f"You've selected {user_race_pick}. \n Next, we shall pick your hero's background.")
 
-	stats_pick_str = random.choice(range(3, 19))
-	stats_pick_dex = random.choice(range(3, 19))
-	stats_pick_con = random.choice(range(3, 19))
-	stats_pick_int = random.choice(range(3, 19))
-	stats_pick_wis = random.choice(range(3, 19))
-	stats_pick_cha = random.choice(range(3, 19))
-	print(f"{user_char_name}'s base attributes shall be: \n STR {stats_pick_str} \n DEX {stats_pick_dex} \n CON {stats_pick_con} \n INT {stats_pick_int} \n WIS {stats_pick_wis} \n CHA {stats_pick_cha} \n Impressive!.")
+	char_bkgd_list = [
+		"Acolyte",
+		"Charlatan",
+		"Criminal",
+		"Entertainer",
+		"Folk Hero",
+		"Gladiator",
+		"Guild Artisan",
+		"Guild Merchant",
+		"Hermit",
+		"Knight",
+		"Noble",
+		"Outlander",
+		"Pirate",
+		"Sage",
+		"Sailor",
+		"Soldier",
+		"Urchin"
+	]
+	user_bkgd_pick = input("Would you like to pick your hero's background? If not, I will choose for you. > ")
+	if user_bkgd_pick != "yes":
+		user_bkgd_pick = random.choice(char_bkgd_list)
+		print(f"Your hero's background shall be {user_bkgd_pick}! \n Next, we shall randomly generate your hero's base attributes.")
+	else:
+		user_bkgd_pick = input("Please pick from the following list: \n Acolyte \n Charlatan \n Criminal \n Entertainer \n Folk Hero \n Gladiator \n Guild Artisan \n Guild Merchant \n Hermit \n Knight \n Noble \n Outlander \n Pirate \n Sage \n Sailor \n Soldier \n Urchin > ")
+		print(f"You've selected {user_bkgd_pick}. \n Next, we shall randomly generate your hero's base attributes.")
 
-	summary = (f"Here's a summary of your new Hero. \n Hero name: {user_char_name} \n Hero class: {user_class_pick} \n Hero race: {user_race_pick} \n Stats: \n STR {stats_pick_str} \n DEX {stats_pick_dex} \n CON {stats_pick_con} \n INT {stats_pick_int} \n WIS {stats_pick_wis} \n CHA {stats_pick_cha}")
+	att_stats = [dice_roll_func() for _ in range(6)]
+	print(f"{user_char_name}'s base attributes shall be: \n STR {att_stats[0]} \n DEX {att_stats[1]} \n CON {att_stats[2]} \n INT {att_stats[3]} \n WIS {att_stats[4]} \n CHA {att_stats[5]} \n Impressive!.")
+
+	summary = (f"Here's a summary of your new Hero. \n Hero name: {user_char_name} \n Hero class: {user_class_pick} \n Hero race: {user_race_pick} \n Hero background: {user_bkgd_pick} \n Stats: \n STR {att_stats[0]} \n DEX {att_stats[1]} \n CON {att_stats[2]} \n INT {att_stats[3]} \n WIS {att_stats[4]} \n CHA {att_stats[5]}")
 	print_summary = input("Would you like a summary of your hero? > ")
 	if print_summary == "yes":
 		print(f"{summary}")
